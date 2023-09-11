8b7ae16eabd26aa88954cb9d75a5f190a673f1e5
Duncan Applegarth <duncana513@gmail.com>
2023-09-11 00:53:39 -0700

    add worker logic for receiving and validating POST request
    * makes sure we only send POST requests to endpoint
    * authenticates request using HMAC sig via verifyRequest()
    * returns appropriate response

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// entrypoint for worker
+
+import { verifyRequest } from './auth';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+  // symmetric key to verify signed request
+  SIGNATURE_KEY: string;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    try {
+      if (request.method !== 'POST') {
+        return new Response('Invalid method.', {
+          status: 400
+        });
+      }
+
+      // authenticate request using HMAC signature
+      const requestBody = (await request.json()) as Record<string, unknown>;
+      const verified = await verifyRequest(requestBody, env.SIGNATURE_KEY);
+      if (!verified) {
+        return new Response('Request is invalid.', {
+          status: 403
+        });
+      }
+
+      const payload = JSON.parse(requestBody.data as string);
+      return new Response(`Request is valid. Payload:\n${JSON.stringify(payload, null, 2)}\n`);
+    } catch (error) {
+      console.error('an error occurred during worker processing:', error);
+      return new Response('Internal Server Error', {
+        status: 500
+      });
+    }
+  }
 };

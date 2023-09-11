ae875bc361c6e15336765ecc43e9abf186db6616
Duncan Applegarth <duncana513@gmail.com>
2023-09-11 00:03:17 -0700

    add try/catch block to verifyRequest
    to catch things like key formatting errors from client and whatnot

--- src/auth.ts
+++ src/auth.ts
@@ -15,41 +15,46 @@ export async function verifyRequest(
   requestBody: Record<string, unknown>,
   secretKey: string
 ): Promise<boolean> {
-  // ensure the correct properties exist
-  if (typeof requestBody.signature !== 'string') {
-    console.error('verifyRequest: signature not present in request as string');
+  try {
+    // ensure the correct properties exist
+    if (typeof requestBody.signature !== 'string') {
+      console.error('verifyRequest: signature not present in request as string');
+      return false;
+    }
+    if (typeof requestBody.data !== 'string') {
+      console.error('verifyRequest: data not present in request as string');
+      return false;
+    }
+
+    const encoder = new TextEncoder();
+
+    // bring in signature
+    const signature = requestBody.signature;
+    const signatureUInt8Array = byteStringToUint8Array(atob(signature));
+
+    // bring in the private symmetric key
+    const key = await crypto.subtle.importKey(
+      'raw',
+      encoder.encode(secretKey),
+      {
+        name: 'HMAC',
+        hash: 'SHA-256'
+      },
+      false,
+      ['verify']
+    );
+
+    // verify that the signatures match the data
+    const verified = await crypto.subtle.verify(
+      'HMAC',
+      key,
+      signatureUInt8Array,
+      encoder.encode(requestBody.data)
+    );
+
+    return verified;
+  } catch (error) {
+    console.error('an error occurred during request verification:\n', (error as Error).stack);
     return false;
   }
-  if (typeof requestBody.data !== 'string') {
-    console.error('verifyRequest: data not present in request as string');
-    return false;
-  }
-
-  const encoder = new TextEncoder();
-
-  // bring in signature
-  const signature = requestBody.signature;
-  const signatureUInt8Array = byteStringToUint8Array(atob(signature));
-
-  // bring in the private symmetric key
-  const key = await crypto.subtle.importKey(
-    'raw',
-    encoder.encode(secretKey),
-    {
-      name: 'HMAC',
-      hash: 'SHA-256'
-    },
-    false,
-    ['verify']
-  );
-
-  // verify that the signatures match the data
-  const verified = await crypto.subtle.verify(
-    'HMAC',
-    key,
-    signatureUInt8Array,
-    encoder.encode(requestBody.data)
-  );
-
-  return verified;
 }

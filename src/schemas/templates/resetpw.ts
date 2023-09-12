4906ddfe2a9dad99bb02ea812f24018214f561d6
Duncan Applegarth <duncana513@gmail.com>
2023-09-11 23:43:59 -0700

    add resetpw email template schema

---
+++ src/schemas/templates/resetpw.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+export const resetPasswordTemplateSchema = z.object({
+  name: z.literal('resetpw'),
+  data: z.object({
+    email: z
+      .string({
+        required_error: 'Email field is required.'
+      })
+      .email({
+        message: 'Email field is not a valid email.'
+      }),
+    token: z.string({
+      required_error: 'Token field is required.'
+    })
+  })
+});
